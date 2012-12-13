d8c2e08539555d71bc2bfaf11d05bae1bf8b46f7
David Taylor <dtaylor@atlassian.com>
2012-12-13 17:57:10 +1100

    Add a dummy service <component> to show how it can be injected into a ContextProvider

--- src/main/java/com/atlassian/confluence/plugins/hello_blueprint/HelloContextProvider.java
+++ src/main/java/com/atlassian/confluence/plugins/hello_blueprint/HelloContextProvider.java
@@ -2,14 +2,24 @@ package com.atlassian.confluence.plugins.hello_blueprint;
 
 import com.atlassian.plugin.PluginParseException;
 import com.atlassian.plugin.web.ContextProvider;
+import com.google.common.collect.Maps;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This provider adds dynamic data for substitution into the content-template.xml \<at:var> elements.
+ *
+ * For the sake of illustration, the provider in turn gets its data from an injected service.
+ */
 public class HelloContextProvider implements ContextProvider
 {
+    private final HelloService helloService;
+
+    public HelloContextProvider(HelloService helloService)
+    {
+        this.helloService = helloService;
+    }
+
     @Override
     public void init(Map<String, String> params) throws PluginParseException
     {
@@ -18,9 +28,11 @@ public class HelloContextProvider implements ContextProvider
     @Override
     public Map<String, Object> getContextMap(Map<String, Object> context)
     {
-        Map<String, Object> result = new HashMap<String, Object>();
+        // Due to a bug, the context is immutable so you have to declare a new Map. This will be fixed in the next
+        // milestone.
+        Map<String, Object> result = Maps.newHashMap();
 
-        result.put("friendlyDate", DateFormat.getDateInstance(DateFormat.LONG).format(new Date()));
+        result.put("friendlyDate", helloService.getFriendlyDate());
 
         return result;
     }
